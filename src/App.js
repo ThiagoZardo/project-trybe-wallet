8da625fd16e03a154b804257fecc9ae635fcf391
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-03-31 14:54:09 -0300

    Iniciando as rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/" component={ Wallet } />
+      {/* <Route path="*" component={ NotFound } /> */}
+    </Switch>
+  );
 }
 
 export default App;
