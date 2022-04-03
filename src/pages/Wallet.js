b2792eee5c2f451c2bb96013dc792506669b2bdc
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-04-03 03:14:01 -0300

    Iniciando o Req08

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -148,16 +148,36 @@ class Wallet extends React.Component {
         </form>
         <table>
           <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
+            <td>Descrição</td>
+            <td>Tag</td>
+            <td>Método de pagamento</td>
+            <td>Valor</td>
+            <td>Moeda</td>
+            <td>Câmbio utilizado</td>
+            <td>Valor convertido</td>
+            <td>Moeda de conversão</td>
+            <td>Editar/Excluir</td>
           </tr>
+          {
+            expensesProp.map((el, index) => (
+              <tr
+                key={ index }
+              >
+                <td>{ el.description }</td>
+                <td>{ el.tag }</td>
+                <td>{ el.method }</td>
+                <td>{ Number(el.value).toFixed(2) }</td>
+                <td>
+                  {
+                    el.currency.exchangeRates
+                      .find((elName) => elName.code === el.currency)
+                      .map((nameCurrent) => nameCurrent.name)
+                  }
+                </td>
+                <td>{ el.tag }</td>
+                <td>{ el.tag }</td>
+              </tr>))
+          }
         </table>
       </nav>
     );
